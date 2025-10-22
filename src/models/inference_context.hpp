0a407b2413517b66eae0807ca620ddb631b1fdbf
zhuyue <zhuyue@qiyuanlab.com>
2025-10-22 17:39:31 +0800

    Use Flash Attention on the opcache_manager.

--- src/models/inference_context.hpp
+++ src/models/inference_context.hpp
@@ -38,6 +38,12 @@ struct InferenceContext {
     void causalSoftmax(std::shared_ptr<Tensor> y,
                        std::shared_ptr<Tensor> x);
 
+    void flashAttention(std::shared_ptr<Tensor> output,
+                       std::shared_ptr<Tensor> query,
+                       std::shared_ptr<Tensor> key,
+                       std::shared_ptr<Tensor> value,
+                       std::shared_ptr<Tensor> mask);
+
     void topkrouter(std::shared_ptr<Tensor> values,  // F32
                     std::shared_ptr<Tensor> indices, // I32
                     std::shared_ptr<Tensor> x,
@@ -109,6 +115,14 @@ inline void rope_v2(std::shared_ptr<Tensor> q, std::shared_ptr<Tensor> k,
 
 inline void causalSoftmax(std::shared_ptr<Tensor> y, std::shared_ptr<Tensor> x) {
     getInferenceContext().causalSoftmax(y, x);
+}
+
+inline void flashAttention(std::shared_ptr<Tensor> output,
+                          std::shared_ptr<Tensor> query,
+                          std::shared_ptr<Tensor> key,
+                          std::shared_ptr<Tensor> value,
+                          std::shared_ptr<Tensor> mask) {
+    getInferenceContext().flashAttention(output, query, key, value, mask);
 }
 
 inline void topkrouter(std::shared_ptr<Tensor> values,  // F32
